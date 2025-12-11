8cfc2d250246b79595eace7827f3325380f352f7
Laszlo Valko <laszlo.valko@origoss.com>
2025-12-11 07:43:34 +0100

    add

--- func.py
+++ func.py
@@ -20,7 +20,18 @@ def handler(ctx, data: io.BytesIO = None):
         headers={"Content-Type": "application/json"}
     )
 
-# List compartments ------------------------------------------------------------
+# Calculate compartment path through parent links
+def get_compartment_path(compartments_by_id, tenancy_id, c):
+    name = c["name"]
+    while True:
+      parent_id = c["parent_id"]
+      if parent_id == tenancy_id:
+        break
+      c = compartments_by_id[parent_id]
+      name = "%s/%s" % (c["name"], name)
+    return name
+
+# List compartments
 def list_compartments(config = {}, **kwargs):
     client = oci.identity.IdentityClient(config=config, **kwargs)
     # OCI API for managing users, groups, compartments, and policies
@@ -33,10 +44,37 @@ def list_compartments(config = {}, **kwargs):
             compartment_id_in_subtree=True,
             access_level='ANY'
         )
-        # Create a list that holds a list of the compartments id and name next to each other
-        compartments = [[c.id, c.name] for c in compartments.data]
+        compartments_by_id = dict()
+        compartments_by_path = dict()
+        for c in compartments.data:
+          compartments_by_id[c.id] = {
+            "id": c.id,
+            "name": c.name,
+            "parent_id": c.compartment_id
+          }
+        for c in compartments_by_id.values():
+          path = get_compartment_path(compartments_by_id, tenancy_id, c)
+          c["path"] = path
+          compartments_by_path[path] = c
+        compartments = [compartments_by_id[c.id] for c in compartments.data]
     except Exception as ex:
         print("ERROR: Cannot access compartments", ex, flush=True)
         raise
     resp = {"compartments": compartments}
+    return resp
+
+# List OKE clusters
+def list_oke_clusters(compartment_id, config = {}, **kwargs):
+    client = oci.container_engine.ContainerEngineClient(config=config, **kwargs)
+    try:
+
+        clusters = client.list_clusters(
+            compartment_id
+        )
+        # Create a list that holds a list of the clusters id and name next to each other
+        clusters = [{ "id": c.id, "name": c.name, "object": c } for c in clusters.data]
+    except Exception as ex:
+        print("ERROR: Cannot access clusters", ex, flush=True)
+        raise
+    resp = {"clusters": clusters}
     return resp
--- test.py
+++ test.py
@@ -2,7 +2,20 @@
 
 import func
 import oci
+import json
 
 config = oci.config.from_file()
-resp = func.list_compartments(config=config)  # function defined below
-print(resp)
+oci.config.validate_config(config)
+compartments = func.list_compartments(config=config)  # function defined below
+for compartment in compartments['compartments']:
+  print("%s: %s" % (compartment['path'], compartment['id']))
+
+found_compartment = None
+for compartment in compartments['compartments']:
+  if compartment["path"] == "enap/cmp-tst":
+    found_compartment = compartment
+    break
+
+clusters = func.list_oke_clusters(compartment["id"], config=config)
+for cluster in clusters['clusters']:
+  print("%s: %s" % (cluster['name'], cluster['id']))
